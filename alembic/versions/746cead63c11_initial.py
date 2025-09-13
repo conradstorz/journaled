fce5918f14916d98fb9261ef8b34ae565684a88f
Conrad Storz <conradstorz@gmail.com>
2025-09-13 12:00:50 -0400

    new database setup

---
+++ alembic/versions/746cead63c11_initial.py
@@ -0,0 +1,26 @@
+
+
+# Alembic migration template
+
+"""initial
+
+Revision ID: 746cead63c11
+Revises: d5cf2219b26c
+Create Date: 2025-09-13 11:56:36.568484
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '746cead63c11'
+down_revision = 'd5cf2219b26c'
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
+
---
+++ alembic/versions/d5cf2219b26c_initial.py
@@ -0,0 +1,26 @@
+
+
+# Alembic migration template
+
+"""initial
+
+Revision ID: d5cf2219b26c
+Revises: 
+Create Date: 2025-09-13 11:53:10.821384
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'd5cf2219b26c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
+
